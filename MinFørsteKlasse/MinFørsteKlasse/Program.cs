dbb7a2c37130a2c9fe7944f7d825fa2e09e54962
daniel matthiesen <drmatthiesen@live.dk>
2019-08-27 07:59:30 +0200

    OOP

---
+++ MinFørsteKlasse/MinFørsteKlasse/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinFørsteKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elev e1 = new Elev();
+            Elev e2 = new Elev();
+
+            e1.Fornavn = "Søren";
+            e1.Efternavn = "Sørensen";
+            e1.Adresse = "Sørens gade 42";
+
+            Console.WriteLine(e1.Fornavn + " " + e1.Efternavn + "\n" + e1.Adresse);
+
+            e2.Fornavn = "Karl";
+            e2.Efternavn = "Karlsen";
+            e2.Adresse = "Karlsgade 1337";
+
+            
+            Console.WriteLine("\n" + e2.Fornavn + " " + e2.Efternavn + "\n" + e2.Adresse);
+
+            Elev e3 = new Elev()
+            {
+                Fornavn = "Hans",
+                Efternavn = "Hansen",
+                Adresse = "Hansens allé 59"
+            };
+
+            Console.WriteLine("\n" + e3.Fornavn + " " + e3.Efternavn + "\n" + e3.Adresse + "\n");
+
+
+            List<int> talListe = new List<int>() { 1, 4, 5, 7, 9, 3, 10 };
+
+            talListe.Add(4);
+
+            foreach (int tal in talListe)
+            {
+                Console.WriteLine(tal);
+            }
+
+            List<Elev> ElevListe = new List<Elev>();
+
+            ElevListe.Add(e3);
+            ElevListe.Add(e2);
+            ElevListe.Add(e1);
+
+            Console.WriteLine();
+
+            foreach (var elev in ElevListe)
+            {
+                Console.Write(elev.Fornavn+" ");
+                Console.WriteLine(elev.Efternavn);
+                Console.WriteLine(elev.Adresse+"\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Elev
+    {
+        public string Fornavn;
+        public string Efternavn;
+        public string Adresse;
+        
+        
+        //private string tlf;
+
+        /*public void AngivTelefonnr(string tlfnr)
+        {
+            tlf = tlfnr;
+        }*/
+
+    }
+}
---
+++ OOP start opgaver/OOP start opgaver/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_start_opgaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Dato;
+            //Opg 3
+            Person p1 = new Person()
+            {
+                Fornavn = "Abraham",
+                Efternavn = "Lincoln"
+            };
+            Person p2 = new Person()
+            {
+                Fornavn = "Gilbert",
+                Efternavn = "Skysovs"
+            };
+            Person p3 = new Person()
+            {
+                Fornavn = "Flomme"
+            };
+            Person p4 = new Person()
+            {
+                Fornavn = "Blomme"
+            };
+            Person p5 = new Person()
+            {
+                Fornavn = "Alexander"
+            };
+
+            //opg 3b
+
+            p3.Efternavn = "";
+            p4.Efternavn = "";
+            p5.Efternavn = "";
+            p1.tlfnr = "";
+            p2.tlfnr = "";
+            p3.tlfnr = "";
+            p4.tlfnr = "";
+            p5.tlfnr = "";
+
+            //Opg 4
+            List<Person> PersonListe = new List<Person>();
+            PersonListe.Add(p1);
+            PersonListe.Add(p2);
+            PersonListe.Add(p3);
+            PersonListe.Add(p4);
+            PersonListe.Add(p5);
+
+            foreach (var person in PersonListe)
+            {
+                int i = 1;
+                Console.Write(i + " " + person.Fornavn + " ");
+                Console.WriteLine(person.Efternavn + "\n");
+                i++;
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+
+    //Opg 1
+    public class Person
+    {
+        //Opg 2
+        public string Fornavn;
+        public string Efternavn;
+        public string tlfnr;
+        private DateTime Fødselsdato;
+
+        //Opg 5
+        public void IndsætDato()
+        {
+            DateTime Dag = Convert.ToDateTime(Console.ReadLine());
+            Fødselsdato = Dag;
+        }
+    }
+}
